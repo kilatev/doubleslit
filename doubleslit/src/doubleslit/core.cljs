f9e4558f90673dc1780e5cc435361279478c94a8
Vitalijs Ozornins <vetalik@gmail.com>
2014-11-12 13:10:00 +0200

    game made

--- doubleslit/src/doubleslit/core.cljs
+++ doubleslit/src/doubleslit/core.cljs
@@ -8,6 +8,8 @@
 ;; (defonce app-data (atom {}))
 
 (println "Edits to this text should ahahah up in your developer console.")
+(let [game (
+      js/Phaser.Game. 800 600 Phaser.CANVAS 'phaser-example' {"preload" preload "create" create})])
 
 (fw/watch-and-reload
  :jsload-callback (fn []
