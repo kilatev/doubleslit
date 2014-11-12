c7d51bbe04a47f3b9fb0e62bef8dad5875613730
Vitalijs Ozornins <vetalik@gmail.com>
2014-11-12 16:41:50 +0200

    adding sprites

--- doubleslit/src/doubleslit/core.cljs
+++ doubleslit/src/doubleslit/core.cljs
@@ -11,6 +11,12 @@
 (def game (
       js/Phaser.Game. 800 600 js/Phaser.CANVAS 'phaser-example' {"preload" preload "create" create}))
 
+(defn preload []
+  (game.load.image 'base' 'images/CONIFERFORESTPACK.jpg'))
+
+(defn create []
+  (game.add.sprite 0 0 'base'))
+
 (fw/watch-and-reload
  :jsload-callback (fn []
                     ;; (stop-and-start-my app)
