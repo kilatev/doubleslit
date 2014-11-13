e980a5fe5e719028ec86c3575081e043f480a27b
Vitalijs Ozornins <vetalik@gmail.com>
2014-11-13 16:04:10 +0200

    trying to load image. still problems

--- doubleslit/src/doubleslit/core.cljs
+++ doubleslit/src/doubleslit/core.cljs
@@ -7,15 +7,16 @@
 ;; define your app data so that it doesn't get over-written on reload
 ;; (defonce app-data (atom {}))
 
+
 (println "Edits to this text shouahahah up in your eloper console.")
 (def game (
-      js/Phaser.Game. 800 600 js/Phaser.CANVAS 'phaser-example' {"preload" preload "create" create}))
+      js/Phaser.Game. 400 200 js/Phaser.CANVAS 'phaser-example' {"preload" doubleslit.core.preload "create" doubleslit.core.create}))
 
-(defn preload []
-  (game.load.image 'base' 'images/CONIFERFORESTPACK.jpg'))
+(def preload (fn []
+  (doubleslit.core.game.load.image 'base' 'images/CONIFERFORESTPACK.jpg')))
 
-(defn create []
-  (game.add.sprite 0 0 'base'))
+(def create (fn []
+  (doubleslit.core.game.add.sprite 0 0 'base')))
 
 (fw/watch-and-reload
  :jsload-callback (fn []
